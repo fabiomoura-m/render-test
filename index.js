f451e3c347ea3e346d582bd6ee83e4819bf13de4
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-12-11 14:07:29 -0300

    refactor routes using mongodb

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const Note = require('./models/note')
+const Note = require('./models/note');
 
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(cors());
-app.use(express.static('dist'));
-
 
 const generateId = () => {
     const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
@@ -25,22 +24,27 @@ app.get('/api/notes', (request, response) => {
     });
 });
 
-app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find(note => note.id === id);
+app.get('/api/notes/:id', (request, response, next) => {
+    const id = request.params.id;
 
-    if (note) {
-        response.json(note);
-    } else {
-        response.status(404).end();
-    }
+    Note.findById(id)
+        .then(note => {
+            if (note) {
+                response.json(note);
+            } else {
+                response.status(404).end();
+            }
+        })
+        .catch(error => next(error));
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id);
-
-    response.status(204).end();
+app.delete('/api/notes/:id', (request, response, next) => {
+    const id = request.params.id;
+    Note.findByIdAndDelete(id)
+        .then(result => {
+            response.status(204).end();
+        })
+        .catch(error => next(error));
 });
 
 app.post('/api/notes', (request, response) => {
@@ -52,16 +56,51 @@ app.post('/api/notes', (request, response) => {
         });
     }
 
+    const note = new Note({
+        content: body.content,
+        important: body.important || false
+    });
+
+    note.save().then(savedNote => {
+        response.json(savedNote);
+    });
+});
+
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body;
+    const id = request.params.id;
+
     const note = {
         content: body.content,
-        important: body.important || false,
-        id: generateId()
+        important: body.important
     };
 
-    notes = notes.concat(note);
-
-    response.json(note);
+    Note.findByIdAndUpdate(id, note, { new: true })
+        .then(updatedNote => {
+            response.json(updatedNote);
+        })
+        .catch(error => next(error));
 });
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' });
+};
+
+// gerenciador de requisições com endpoint desconhecido
+app.use(unknownEndpoint);
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' });
+    }
+
+    next(error);
+};
+
+// Este deve ser o último middleware a ser carregado.
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
