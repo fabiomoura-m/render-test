69dfae4458b4431579c373af874c15e42f30ad71
Fabio de Moura Martins <fabio.mmouras@hotmail.com>
2023-12-11 10:30:03 -0300

    add mongodb to project

--- index.js
+++ index.js
@@ -1,42 +1,28 @@
+require('dotenv').config();
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
+
 const app = express();
+const Note = require('./models/note')
 
 app.use(express.json());
-app.use(cors())
-app.use(express.static('dist'))
+app.use(cors());
+app.use(express.static('dist'));
 
-let notes = [
-    {
-        id: 1,
-        content: 'HTML is easy',
-        important: true
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only JavaScript',
-        important: false
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        important: true
-    }
-];
 
 const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-  }
+    const maxId = notes.length > 0 ? Math.max(...notes.map(n => n.id)) : 0;
+    return maxId + 1;
+};
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!!</h1>');
 });
 
 app.get('/api/notes', (request, response) => {
-    response.json(notes);
+    Note.find({}).then(notes => {
+        response.json(notes);
+    });
 });
 
 app.get('/api/notes/:id', (request, response) => {
@@ -54,30 +40,30 @@ app.delete('/api/notes/:id', (request, response) => {
     const id = Number(request.params.id);
     notes = notes.filter(note => note.id !== id);
 
-    response.status(204).end()
+    response.status(204).end();
 });
 
 app.post('/api/notes', (request, response) => {
-    const body = request.body
+    const body = request.body;
 
     if (!body.content) {
-      return response.status(400).json({ 
-        error: 'content missing' 
-      })
+        return response.status(400).json({
+            error: 'content missing'
+        });
     }
-  
-    const note = {
-      content: body.content,
-      important: body.important || false,
-      id: generateId(),
-    }
-  
-    notes = notes.concat(note)
-  
-    response.json(note)
-  })
 
-const PORT = process.env.PORT || 3001
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId()
+    };
+
+    notes = notes.concat(note);
+
+    response.json(note);
+});
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
